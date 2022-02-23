f46a628f49ad4f1eea0f589fda790c347474f3be
shadawcraw <alexandre1920911@gmail.com>
2022-02-23 13:01:59 +0200

    Add more tests

--- src/tests.rs
+++ src/tests.rs
@@ -7,14 +7,42 @@ fn test_to_int() {
 }
 
 #[test]
+#[should_panic]
+fn test_to_int_fail() {
+    let s = String::from("foo");
+    assert_eq!(to_int(&s), 0);
+}
+
+#[test]
+fn test_to_int_negative() {
+    let s = String::from("-123");
+    assert_eq!(to_int(&s), -123);
+}
+
+#[test]
 fn test_is_numeric() {
     let s = String::from("123");
     assert_eq!(check_if_numeric(&s), true);
 }
 
 #[test]
+fn test_is_numeric_negative() {
+    let s = String::from("-123");
+    assert_eq!(check_if_numeric(&s), true);
+}
+
+
+
+#[test]
 #[should_panic]
-fn test_is_numeric_fail() {
+fn test_is_numeric_alpha_and_numeric_fail() {
     let s = String::from("123a");
+    check_if_numeric(&s);
+}
+
+#[test]
+#[should_panic]
+fn test_is_numeric_alpha_only_fail() {
+    let s = String::from("foo");
     check_if_numeric(&s);
 }
