08597a60f55126f9f4b149743343f33943883f2b
shadawcraw <alexandre1920911@gmail.com>
2022-02-23 12:43:48 +0200

    Add documentation comments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::io;
 use rand::Rng;
  
+/// ==================
+/// == Main Program ==
+/// ==================
+
 fn main() {
     let mut rng = rand::thread_rng();
-    let to_guess: i8 = rng.gen_range(0..100); // generate the number to guess
+    let to_guess: i8 = rng.gen_range(0..100); // Generate the random number to guess
     println!("Hi! I'm going to guess a number between 0 and 100 and you're going to have to guess it. Ok?");
 
     let mut first_guess: bool = true;
     
-    loop { // ask for input until the user guesses correctly.
+    loop { // Ask for input until the user guesses correctly.
         let guess = ask(first_guess);
         first_guess = false;
         check_if_numeric(&guess);
@@ -24,11 +28,24 @@ fn main() {
     }
 }
 
-fn to_int(s: &String) -> i32 { // self explanatory name.
+// =======================
+// == Utility Functions ==
+// =======================
+
+/// Converts a String to a i32.
+///
+/// # Examples
+///
+/// ```
+/// let s = String::from("123");
+/// assert_eq!(to_int(&s), 123);
+/// ```
+fn to_int(s: &String) -> i32 {
     s.parse().unwrap()
 }
 
-fn ask(first_time: bool) -> String { // ask the user for input
+/// Ask the user for input.
+fn ask(first_time: bool) -> String {
     let mut guess = String::new();
 
     if first_time {
@@ -36,14 +53,29 @@ fn ask(first_time: bool) -> String { // ask the user for input
     }
 
     io::stdin().read_line(&mut guess).expect("Error!"); // read input
-    guess.pop(); // do this because for some reason Rust adds another character at the end of string (might be the escape character)
+    guess.pop(); // Do this because for some reason Rust adds another character at the end of string (might be the escape character)
     return guess;
 }
 
-fn check_if_numeric(s: &String) -> bool { // check if all characters are numbers, panic if not.
-    for c in s.chars() {
+/// Check if all characters provided are numbers, panic if not.
+/// 
+/// # Panics
+/// 
+/// Panics if `s` contains non-numeric chars.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let s = String::from("123");
+/// assert_eq!(check_if_numeric(&s), true);
+/// 
+/// let s = String::from("123a");
+/// assert_eq!(check_if_numeric(&s), false); // panics with "Invalid Characters! Please only input numbers."
+/// ```
+fn check_if_numeric(s: &String) -> bool {
+    for c in s.chars() {  // Loop through all the chars in the String.
         if !c.is_numeric() {
-            panic!("Invalid Characters! Please input a number");
+            panic!("Invalid Characters! Please only input numbers.");
         }
     }
     return true;
